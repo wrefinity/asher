wrefinity/asher_backy

notfications, tenancy files, docker and docker compose creation

src/controllers/apartment.controller.ts
@@ -40,7 +40,26 @@ class AppartmentController {
             ErrorService.handleError(error, res)
         }
     }
-
+    async getAppartmentById(req: Request, res: Response) {
+        try {
+            const apartmentId = req.params.apartmentId;
+            const apartment = await ApartmentServices.getApartmentById(apartmentId)
+            if (!apartment) return res.status(200).json({message: "No Appartment Found"})
+            return res.status(200).json(apartment)
+        } catch (error) {
+            ErrorService.handleError(error, res)
+        }
+    }
+    deleteApartments = async(req:Request, res:Response) =>{
+        try {
+            const apartmentId = req.params.apartmentId;
+            const apartment = await ApartmentServices.deleteApartment(apartmentId)
+            if (!apartment) return res.status(404).json({ error: 'apartment not found' });
+            res.status(200).json({ message: 'appartment deleted successfully' });
+        } catch (error) {
+            ErrorService.handleError(error, res)
+        }
+    }
 
 }
 

src/index.ts
@@ -1,8 +1,8 @@
 import express, { Express, } from "express";
 import session from "express-session";
 import cookieParser from 'cookie-parser'
-// import passport from "passport";
 import { PORT, APP_SECRET } from "./secrets";
+import connectDB from "./db/mongo_connnect"
 import AuthRouter from "./routes/auth"
 import ProfileRouter from "./routes/profile"
 import StatusRouter from "./routes/status"
@@ -12,6 +12,8 @@ import EmailRouter from "./routes/email"
 import PropertyRouter from "./routes/property"
 import CategoryRouter from "./routes/category"
 import TransactionRouter from "./routes/transaction"
+import FilesRouter from "./routes/files"
+import NotificationRouter from "./routes/notification"
 
 import VendorServiceRouter from "./routes/services"
 import MaintenanceRouter from "./routes/maintenance"
@@ -36,6 +38,7 @@ class Server {
         this.app = express();
         this.port = port;
         this.appSecret = secret;
+        connectDB();
         this.configureMiddlewares();
         this.configureRoutes();
     }
@@ -57,6 +60,7 @@ class Server {
         this.app.get("/", (req, res) => res.json({ message: "it is working" }));
         this.app.use("/api/auth", AuthRouter);
         this.app.use("/api/status", StatusRouter);
+        this.app.use("/api/notification", NotificationRouter)
         this.app.use("/api/categories", CategoryRouter)
         this.app.use("/api/profile", ProfileRouter);
         this.app.use("/api/vendor/services", VendorServiceRouter);
@@ -67,6 +71,7 @@ class Server {
         this.app.use("/api/maintenance", MaintenanceRouter);
         this.app.use("/api/community-post", CommunityPostRouter)
         this.app.use("/api/tenants/community", communityRoutes);
+        this.app.use("/api/tenants/files", FilesRouter);
         this.app.use("/api/ads", AdsRouter);
         this.app.use("/api/transactions", TransactionRouter);
         this.app.post('/paystack/webhook', (req, res) => paystackServices.handleWebhook(req, res))

src/utils/types.ts
@@ -12,6 +12,12 @@ export interface CustomRequest extends Request {
     files: {
         [fieldname: string]: Express.Multer.File[];
     };
+    params:{
+        maintenanceId?:string;
+        apartmentId?:string;
+        propertyId?:string;
+        id?:string;
+    }
 }
 
 export type EmailDataType = {
@@ -93,9 +99,9 @@ export interface WebHookData {
     fees_split: any | null;
     authorization: Authorization;
     customer: Customer;
-    plan: object; // Assuming plan is an object, specify further if needed
-    subaccount: object; // Assuming subaccount is an object, specify further if needed
-    split: object; // Assuming split is an object, specify further if needed
+    plan: object;
+    subaccount: object;
+    split: object;
     order_id: string | null;
     paidAt: string;
     requested_amount: number;

wrefinity/asher

src/controllers/apartment.controller.ts
@@ -1,13 +1,14 @@
-import { Request, Response } from "express";
+import { Response } from "express";
 import ErrorService from "../services/error.service";
 import ApartmentServices from "../services/apartment.services";
 import { apartmentSchema } from "../validations/schemas/apartment.schema";
+import { CustomRequest } from "../utils/types";
 
 
 class AppartmentController {
     constructor() { }
 
-    async createApartment(req: Request, res: Response) {
+    async createApartment(req: CustomRequest, res: Response) {
         try {
             const { error } = apartmentSchema.validate(req.body);
             if (error) return res.status(400).send(error.details[0].message);
@@ -30,7 +31,7 @@ class AppartmentController {
             ErrorService.handleError(error, res)
         }
     }
-    async getAppartments(req: Request, res: Response) {
+    async getAppartments(req: CustomRequest, res: Response) {
         try {
             const propertyId = req.params.propertyId;
             const properties = await ApartmentServices.getApartments(propertyId)
@@ -40,7 +41,26 @@ class AppartmentController {
             ErrorService.handleError(error, res)
         }
     }
-
+    async getAppartmentById(req: CustomRequest, res: Response) {
+        try {
+            const apartmentId = req.params.apartmentId;
+            const apartment = await ApartmentServices.getApartmentById(apartmentId)
+            if (!apartment) return res.status(200).json({message: "No Appartment Found"})
+            return res.status(200).json(apartment)
+        } catch (error) {
+            ErrorService.handleError(error, res)
+        }
+    }
+    deleteApartments = async(req:CustomRequest, res:Response) =>{
+        try {
+            const apartmentId = req.params.apartmentId;
+            const apartment = await ApartmentServices.deleteApartment(apartmentId)
+            if (!apartment) return res.status(404).json({ error: 'apartment not found' });
+            res.status(200).json({ message: 'appartment deleted successfully' });
+        } catch (error) {
+            ErrorService.handleError(error, res)
+        }
+    }
 
 }
 

src/index.ts
@@ -1,21 +1,23 @@
 import cookieParser from 'cookie-parser';
 import express, { Express } from "express";
 import session from "express-session";
+// import passport from "passport";
+import connectDB from './db/mongo_connnect';
+import { PORT, APP_SECRET } from "./secrets";
+import AuthRouter from "./routes/auth"
+import ProfileRouter from "./routes/profile"
+import StatusRouter from "./routes/status"
+import ApplicationRouter from "./routes/applicant"
+import ChatRoomRouter from "./routes/chats"
+import EmailRouter from "./routes/email"
+import PropertyRouter from "./routes/property"
+import CategoryRouter from "./routes/category"
+import TransactionRouter from "./routes/transaction"
+import NotificationRouter from "./routes/notification"
 
-import ApplicationRouter from "./routes/applicant";
-import AuthRouter from "./routes/auth";
-import CategoryRouter from "./routes/category";
-import ChatRoomRouter from "./routes/chats";
-import EmailRouter from "./routes/email";
-import ProfileRouter from "./routes/profile";
-import PropertyRouter from "./routes/property";
-import StatusRouter from "./routes/status";
-import TransactionRouter from "./routes/transaction";
-import { APP_SECRET, PORT } from "./secrets";
-
+import VendorServiceRouter from "./routes/services"
+import MaintenanceRouter from "./routes/maintenance"
 import { PrismaClient } from "@prisma/client";
-import MaintenanceRouter from "./routes/maintenance";
-import VendorServiceRouter from "./routes/services";
 import WalletRouter from "./routes/wallet";
 import paystackServices from "./services/paystack.services";
 import AdsRouter from "./tenant/routes/ads.routes";
@@ -41,6 +43,7 @@ class Server {
         this.app = express();
         this.port = port;
         this.appSecret = secret;
+        connectDB();
         this.configureMiddlewares();
         this.configureRoutes();
     }
@@ -63,6 +66,7 @@ class Server {
         this.app.post("/paystack/webhook", (req, res) => paystackServices.handleWebhook(req, res))
         this.app.use("/api/auth", AuthRouter);
         this.app.use("/api/status", StatusRouter);
+        this.app.use("/api/notification", NotificationRouter)
         this.app.use("/api/categories", CategoryRouter)
         this.app.use("/api/profile", ProfileRouter);
         this.app.use("/api/vendor/services", VendorServiceRouter);
@@ -73,7 +77,6 @@ class Server {
         this.app.use("/api/maintenance", MaintenanceRouter);
         this.app.use("/api/community-post", CommunityPostRouter)
         this.app.use("/api/tenants/community", communityRoutes);
-        this.app.use("/api/support", SupportRouter);
         this.app.use("/api/ads", AdsRouter);
         this.app.use("/api/transactions", TransactionRouter);
         this.app.use("/api/wallet", WalletRouter);

src/utils/types.ts
@@ -12,6 +12,27 @@ export interface CustomRequest extends Request {
     files: {
         [fieldname: string]: Express.Multer.File[];
     };
+    params: {
+
+        maintenanceId?: string;
+        applicationId?: string;
+        apartmentId?: string;
+        propertyId?: string;
+        propertiesId?: string;
+        chatRoomId?: string;
+        inviteCode?: string;
+        ticketId?: string;
+        categoryId?: string;
+        communityId?: string;
+        receiverId?: string;
+        userId?: string;
+        communityPostId?: string;
+        emailId?: string;
+        profileId?: string;
+        adsId?: string;
+        id?: string;
+
+    }
 }
 
 export type EmailDataType = {
@@ -93,9 +114,9 @@ export interface WebHookData {
     fees_split: any | null;
     authorization: Authorization;
     customer: Customer;
-    plan: object; // Assuming plan is an object, specify further if needed
-    subaccount: object; // Assuming subaccount is an object, specify further if needed
-    split: object; // Assuming split is an object, specify further if needed
+    plan: object;
+    subaccount: object;
+    split: object;
     order_id: string | null;
     paidAt: string;
     requested_amount: number;